daf11d291a8f8579e0da68a205886fe4dda639cb
Muskan Saini <muskansaini@Muskans-MacBook-Air.local>
2024-06-22 12:08:41 +0530

    define all the variables name with example

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import Counter from "./Counter";
-import UseStateWithArray from "./UseStateWithArray";
-import UseStateWithArrayOfObject from "./UseStateWithArrayOfObject";
-import UseStateWithString from "./UseStateWithString";
+import Counter from "./components/Counter";
+import UseStateWithArray from "./components/UseStateWithArray";
+import UseStateWithArrayOfObject from "./components/UseStateWithArrayOfObject";
+import UseStateWithString from "./components/UseStateWithString";
 
 function App() {
   return (
     <>
       {/* Component demonstrating the use of useState with a string */}
       <UseStateWithString />
-      
+
       {/* Component demonstrating the use of useState with a simple array */}
       <UseStateWithArray />
-      
+
       {/* Component demonstrating the use of useState with an array of objects */}
       <UseStateWithArrayOfObject />
-      
+
       {/* Component demonstrating a simple counter using useState */}
-      <Counter/>
+      <Counter />
     </>
   );
 }
