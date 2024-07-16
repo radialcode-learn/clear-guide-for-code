11699ef112f206dd125be97f0b6391d1f7c8b80c
Sumit007Sharma <48888437+Sumit007Sharma@users.noreply.github.com>
2024-07-17 04:37:56 +0530

    TODOs

--- src/views/Homepage.jsx
+++ src/views/Homepage.jsx
@@ -4,6 +4,8 @@ import About from "../components/About";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 
+// TODO: We can remove the <div> with <>
+// Define, utils folder
 const Homepage = () => {
   return (
     <div>
